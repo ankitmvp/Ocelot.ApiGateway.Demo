d0dc69bbe989ffc3af65b5d9f77845994bf1695e
Ankit Bansal <ankitbansalniit@outlook.com>
2021-09-20 20:12:18 +0530

    Initial commit ...

---
+++ Ocelot.ApiGateway.Demo/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.ApiGateway.Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        private readonly string _policyName = "AllowAnyOriginMethodHeader";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            var allowedHosts = new List<string>();
+            var configHosts = Configuration["AllowedCorsPolicy"];
+            if (!string.IsNullOrWhiteSpace(configHosts))
+            {
+                allowedHosts.AddRange(configHosts.Split(',', StringSplitOptions.RemoveEmptyEntries));
+            }
+            services.AddCors(o =>
+            {
+                o.AddPolicy(
+                    _policyName,
+                    x =>
+                    {
+                        x.WithOrigins(allowedHosts.ToArray())
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                    }
+                    );
+            });
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(o =>
+                {
+                    o.Authority = Configuration["Authentication:Authority"];
+                    o.RequireHttpsMetadata = true;
+                    o.LegacyAudienceValidation = true;
+                }
+                );
+
+            services.AddOcelot();
+            services.AddApplicationInsightsTelemetry(Configuration["InstrumentationKey"]);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(_policyName);
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseWebSockets();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseOcelot();
+        }
+    }
+}
